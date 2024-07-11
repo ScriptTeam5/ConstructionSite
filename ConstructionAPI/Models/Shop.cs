8f897924ad050531fcca99bf6fdcc3beb11d76c1
IlkinNovruzov <inovruzov2004@gmail.com>
2024-07-11 18:41:23 +0400

    firstcommit

---
+++ ConstructionAPI/Models/Shop.cs
@@ -0,0 +1,19 @@
+﻿using ConstructionAPI.DAL;
+
+namespace ConstructionAPI.Models
+{
+    public class Shop : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+        public string UserId { get; set; }
+        public User User { get; set; }
+
+        public List<Product> Products { get; set; }
+
+    }
+}
---
+++ ConstructionAPI/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ConstructionAPI.Models
+{
+    public class User:IdentityUser<int>
+    {
+        public string UserName {  get; set; }
+        public string PhoneNumber {  get; set; }
+        public int? ShopId { get; set; }
+        public Shop Shop { get; set; }
+
+        public List<Product> Products { get; set; }
+
+    }
+}
